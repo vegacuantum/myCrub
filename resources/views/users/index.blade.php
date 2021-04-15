f6a34efc9d80404f45bcfbe244019ebac924465e
Emilio Vega <69390844+vegacuantum@users.noreply.github.com>
2021-04-15 04:16:37 -0500

    Update index.blade.php
    Agregue un titulo

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="/styles/styles.css">
-    <title>Educuantico</title>
+    <title>Practicando</title>
   </head>
   <body>
        
@@ -17,6 +17,7 @@
             <div class="row">
                 <div class="col-sm-8 mx-auto">
                   <div class="card">
+                  <h1>Crear un usuarios</h>
                     <form action="{{ route('store') }}" method="POST">
                       <div class="form-row">
                         <div class="col-sm-3">
